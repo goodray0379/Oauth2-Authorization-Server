71f51c33058b5c6563677033a437c1118ce1fd81
heoyoungmin <goodray0379@gmail.com>
2021-10-26 23:29:19 +0900

    LOG 세팅 기본 설정 및 테스트

--- src/main/java/com/devmin/userservice/web/TestController.java
+++ src/main/java/com/devmin/userservice/web/TestController.java
@@ -1,13 +1,22 @@
 package com.devmin.userservice.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/test")
     public String hello() {
+        logger.trace("TRACE 로그 메시지");
+        logger.debug("DEBUG 로그 메시지");
+        logger.info("INFO 로그 메시지");
+        logger.warn("WARN 로그 메시지");
+        logger.error("ERROR 로그 메시지");
         return "test";
     }
 }
