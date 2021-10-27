4ee4dfc2e70fc2bc13d3da10e43a7f1be76976ae
heoyoungmin <goodray0379@gmail.com>
2021-10-27 23:45:29 +0900

    user domain 설정 및 base entity 설정

---
+++ src/main/java/com/devmin/userservice/domain/users/User.java
@@ -0,0 +1,40 @@
+package com.devmin.userservice.domain.users;
+
+import com.devmin.userservice.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 12, nullable = false)
+    private String username;
+
+    @Column(length = 20, nullable = false)
+    private String password;
+
+    @ColumnDefault("user")
+    @Column(nullable = false)
+    private String role;
+
+    @Column(columnDefinition = "TEXT")
+    private String introduction;
+
+    @Builder
+    public User(String username, String password, String role, String introduction) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.introduction = introduction;
+    }
+}
